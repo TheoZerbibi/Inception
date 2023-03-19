d460462bd4742440527863359c2e4010341efe65
TheoZerbibi <tzeribi@gmail.com>
2023-03-19 06:28:06 +0000

    Bonus - Start static site

---
+++ srcs/requirements/bonus/static/site/src/index.ts
@@ -0,0 +1,66 @@
+import express from "express";
+import path from "path";
+import bodyParser from 'body-parser';
+import { curly } from 'node-libcurl';
+
+const app = express();
+const port = 4242;
+
+app.set('views', `${__dirname}/views`);
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let	isLog:boolean = false;
+let login:string = 'thzeribi';
+let	stud:any = null;
+
+app.get("/", (req:any, res:any) => {
+	if (stud && isLog)
+		res.redirect('/info');
+	else
+		res.render('pages/index');
+});
+
+app.get("/info", (req:any, res:any) => {
+	if (stud && isLog)
+		res.render('pages/info', {
+			user: stud
+		});
+	else
+		res.redirect('/');
+});
+
+app.post('/info', async (req:any, res:any) => {
+	if (req.body.login && req.body.login != login && isLog)
+		await getData(req.body.login);
+	res.redirect('/info');
+});
+
+app.post('/', async (req:any, res:any) => {
+	if (req.body.password && req.body.password == "42Cursus")
+	{
+		isLog = true;
+		await getData(login);
+	}
+	res.redirect('/info');
+});
+
+async function getData(_login:string)
+{
+	await curly.get(`https://proxy42.tzeribi.fr/api/users/${_login}`, {httpHeader: ['Authorization: 3Bj4v5*paNhJ',]}).then(async (response) => {
+		if (response.statusCode != 200) {
+			login = 'norminet';
+			await getData(login);
+		} else
+			stud = response.data;
+	});
+}
+
+
+app.listen(port, () => {
+	console.log(`server started at http://localhost:${port}`);
+});
